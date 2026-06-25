8bec2d45984e3dfb3e3a1c16bec1b0bfba271675
matias <matiasmv2005@gmail.com>
2026-06-25 17:33:44 -0400

    actualizaciones

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const BASE_URL =
   import.meta.env.VITE_API_URL ||
-  'http://localhost:5044/api/';
+  'https://backendnexo-production.up.railway.app/api/';
 
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
